5b9000d6c0aada501df91335f338e154699c0a9a
constantino Gallusi msigwa <80386833+Constantino2019-et@users.noreply.github.com>
2021-03-18 11:22:30 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <wiringPi.h>
+#include <iostream>
+using namespace std;
+#define LED_GPIO 17
+int main() {
+wiringPiSetupGpio();
+cout << "Starting fast GPIO toggle on GPIO" << LED_GPIO << endl;
+cout << "Press CTRL+C to quit..." << endl;
+pinMode(LED_GPIO, OUTPUT);
+while(1) {
+digitalWrite(LED_GPIO, HIGH);
+delay(1000);
+digitalWrite(LED_GPIO, LOW);
+delay(1000);
+}
+return 0;
+}
